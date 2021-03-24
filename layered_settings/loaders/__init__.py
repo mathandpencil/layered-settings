d5eb546de402b2788d0a0c11225e831bbf33dd07
Scott Stafford <scott.stafford@gmail.com>
2021-03-24 18:37:02 -0400

    Improve logging.

--- layered_settings/loaders/__init__.py
+++ layered_settings/loaders/__init__.py
@@ -1,4 +1,5 @@
 from .base_loader import BaseLoader
+from .callable_loader import CallableLoader
 from .dict_loader import DictLoader
 from .env_loader import EnvLoader
 from .ssm_loader import SSMLoader
