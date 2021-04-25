8c3a48cc5ac7436a4ac7c1a72e81dad0e58ac7d8
Scott Stafford <scott.stafford@gmail.com>
2021-04-25 18:41:28 -0400

    v1.0.5 - Added case insensitive env search.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 ## [Unreleased]
 
+## [1.0.5] - 2021-04-25
+
+### Changed
+
+-   Made Env explicitly case insensitive.
+
 ## [1.0.3] - 2021-03-23
 
 ### Changed
@@ -26,7 +32,9 @@
 
 -   Initial release.
 
-[unreleased]: https://github.com/mathandpencil/layered-settings/compare/v1.0.2..HEAD
+[unreleased]: https://github.com/mathandpencil/layered-settings/compare/v1.0.5..HEAD
+[1.0.5]: https://github.com/mathandpencil/layered-settings/releases/tag/v1.0.5
+[1.0.3]: https://github.com/mathandpencil/layered-settings/releases/tag/v1.0.3
 [1.0.2]: https://github.com/mathandpencil/layered-settings/releases/tag/v1.0.2
 [1.0.1]: https://github.com/mathandpencil/layered-settings/releases/tag/v1.0.1
 [1.0.0]: https://github.com/mathandpencil/layered-settings/releases/tag/v1.0.0
--- layered_settings/loaders/env_loader.py
+++ layered_settings/loaders/env_loader.py
@@ -8,14 +8,25 @@ logger = logging.getLogger(__name__)
 
 class EnvLoader(BaseLoader):
     """ Load from the os.environ.  Pass a format string containing {section} and {key} markers, such as
-    'APP__{section}__{key}' or just '{key}'.""" 
+    'APP__{section}__{key}' or just '{key}'.  Defaults to a case-insensitive search through environment variables,
+    since env vars are typically uppercase and sections are typically lowercase, plus it's more Windows-friendly.
+    Use is_case_sensitive=True to be case sensitive.""" 
     
-    def __init__(self, format):
+    def __init__(self, format, is_case_sensitive=False):
         self.env_var_format = format
+        self.is_case_sensitive = is_case_sensitive
 
     def get_setting(self, section, key):
         s = self.env_var_format.format(section=section, key=key)
-        return os.environ[s]
+
+        if self.is_case_sensitive:
+           return os.environ[s]
+        else:
+            s = s.lower()
+            for k, v in os.environ.items():
+                if k.lower() == s:
+                    return v
+            raise KeyError(k)
 
     def __str__(self):
         return f"env var using {self.env_var_format}"
